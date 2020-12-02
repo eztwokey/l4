7bc171caaecb9e051cec7deaa69451f32ef8fdc1
eztwokey <eztwokey@bk.ru>
2020-12-02 14:31:06 +0300

    a

---
+++ ind1.py
@@ -0,0 +1,11 @@
+m = int(input('Введите номер времени года'))
+if m == 1:
+    print ('Зима')
+elif m == 2:
+    print ('Весна')
+elif m == 3:
+    print ('Лето')
+elif m == 4:
+    print ('Осень')
+else:
+    print ('Error')
